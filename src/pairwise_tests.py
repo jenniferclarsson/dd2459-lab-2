43b2f2985dea32eba12d61e50240b63006daae3d
jenniferclarsson <jennifer.2.larsson@gmail.com>
2023-02-17 11:12:58 +0100

    fix pairwise test

--- src/pairwise_tests.py
+++ src/pairwise_tests.py
@@ -1,24 +1,35 @@
 from unittest import main, TestCase
-from allpairspy import AllPairs
 from main import *
 
 class test_search(TestCase):
 
     def setUp(self):
-        self.arrays = [[1, 10], [1, 3, 5, 5, 10]]
-        self.keys = [1, 0, 10]
-        self.parameters =[self.arrays, self.keys]
+        self.key = 0
+        self.bad_key = 2
+        self.n = 10
+        self.numArrays = self.n**2+self.n+1
+        self.arrays = [[0 for i in range(self.n)] for j in range(self.numArrays)]
+        for i in range(1, self.n+1):
+            self.arrays[i][i-1] = 1
 
-    def test(self):
-        for i, pairs in enumerate(AllPairs(self.parameters)):
-            res = member(pairs[0], pairs[1])
-            if pairs[1] == 1:
-                self.assertTrue(res)
-            elif pairs[1] == 10:
-                self.assertTrue(res)
-            else:
-                self.assertFalse(res)
+        i = self.n+1
+        a = 0
+        for k in range(self.n):
+            for j in range(i+k*self.n, i+(k+1)*self.n):
+                self.arrays[j][k] = 1
+                self.arrays[j][a] = -1
+                a += 1
+            a = 0
 
+    def test_key_exists(self):
+        for i in range(self.numArrays):
+            print("TEST RUN good key",  i, member(self.arrays[i], self.key))
+            self.assertTrue(member(self.arrays[i], self.key))
+
+    def test_key_does_not_exist(self):
+        for i in range(self.numArrays):
+            print("TEST RUN bad key",  i, not(member(self.arrays[i], self.bad_key)))
+            self.assertFalse(member(self.arrays[i], self.bad_key))
 
 if __name__ == "__main__":
     main()
